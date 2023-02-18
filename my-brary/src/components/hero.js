94fec898787e3cc8372169a11e2795aa171dc08d
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-18 23:58:10 +0530

    Header Finished

--- my-brary/src/components/hero.js
+++ my-brary/src/components/hero.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='hero'></div>
+    <div className='hero'>
+      <div>
+        <h1>Build your library</h1>
+        <p>Buy two selected book and get one for free</p>
+      </div>
+      <button>View all</button>
+      <div>
+        <img src="assets/hero.svg"></img>
+      </div>
+    </div>
   )
 }
 
