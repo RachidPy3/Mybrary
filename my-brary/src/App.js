ea94ee3dd566b87140ef65fb40952c94bbe7efaa
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-18 21:10:26 +0530

    Basic Structure

--- my-brary/src/App.js
+++ my-brary/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I am testing to push the code.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <Hero/>
     </div>
   );
 }
