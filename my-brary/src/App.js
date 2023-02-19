3e083feb5c5d5966e9a394f9ef749c1b0b1eadaa
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-19 22:45:43 +0530

    Fixed Navigations Issue

--- my-brary/src/App.js
+++ my-brary/src/App.js
@@ -6,30 +6,21 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TrendingPage from "./pages/TrendingPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [bookData, setBookData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://openlibrary.org/trending/daily.json")
-      .then((response) => {
-        console.log(response.data.works);
-        setBookData(() => response.data.works);
-      });
-  }, []);
+  
 
   return (
     <div className="app">
       <Router>
         <Navigation />
         <Routes>
+          <Route path="/" element = {<HomePage/>}/>
           <Route path="/Trending" element={<TrendingPage />} />
         </Routes>
       </Router>
-      <div className="con">
-        <Hero />
-        <Trending trending={bookData.slice(0, 12)} />
-      </div>
+      
     </div>
   );
 }
