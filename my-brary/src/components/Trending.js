010a2f3d1b8b5c5e54619c9099ead4f49441cf7c
rachidPy3 <mouatacim.med@gmail.com>
2023-02-19 13:58:12 +0100

    fixed some ui bugs

--- my-brary/src/components/Trending.js
+++ my-brary/src/components/Trending.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Trending.css'
-import { BsStarFill } from 'react-icons/bs';
+import React from "react";
+import "./Trending.css";
+import { BsStarFill } from "react-icons/bs";
 
-export default function Trending({trending}) {
+export default function Trending({ trending }) {
   return (
-    <div className='trending'>
-        <div className='trending-header'><h3>Trending</h3><p>View All</p></div>
+    <div className="trending">
+      <div className="trending-header">
+        <h3>Trending</h3>
+        <p>View All</p>
+      </div>
 
-        <div className='grid'>
-
-            {trending.map((p)=>(
-                <div className='single-grid'>
-                <img src='assets/cover.jpg'></img>
-                <div>
-                    <p className='title'>{p.title}</p>
-                    <p className='author'>{p.author}</p>
-                    <p className='pubDate'>{p.first_publish_year}</p>
-                </div>
+      <div className="grid">
+        {trending.map((p) => (
+          <div className="single-grid">
+            <img src="assets/cover.jpg"></img>
+            <div>
+              <p className="title">{p.title}</p>
+              <p className="author">{p.author}</p>
+              <p className="pubDate">{p.first_publish_year}</p>
             </div>
-                
-            ))}
-        
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
