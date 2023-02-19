b6242713096f3e136ee10a4c945bc1ceb7e5180c
rachidPy3 <mouatacim.med@gmail.com>
2023-02-19 13:40:04 +0100

    code formatting

--- my-brary/src/App.js
+++ my-brary/src/App.js
@@ -1,33 +1,28 @@
-
-import Navigation from './components/navigation';
-import Hero from './components/hero';
-import './styles.css'
-import Trending from './components/Trending';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Navigation from "./components/navigation";
+import Hero from "./components/hero";
+import "./styles.css";
+import Trending from "./components/Trending";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [bookData, setBookData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://openlibrary.org/trending/daily.json")
+      .then((response) => {
+        console.log(response.data.works);
+        setBookData(() => response.data.works);
+      });
+  }, []);
 
- const [bookData , setBookData] = useState([])
- useEffect(()=>{
-   axios.get("https://openlibrary.org/trending/daily.json").then( (response) =>{
-      console.log(response.data.works);
-      setBookData(()=> response.data.works)
-    })
- }, [])
-
- 
- 
-
-
-const trending =  bookData.works;
+  const trending = bookData.works;
 
   return (
     <div className="App">
-      <Navigation/>
-      <Hero/>
-    <Trending trending={bookData.slice(0,12)}/>
+      <Navigation />
+      <Hero />
+      <Trending trending={bookData.slice(0, 12)} />
     </div>
   );
 }
