cddb1d00b83db18114c868a664c6b0e930632927
rachidPy3 <mouatacim.med@gmail.com>
2023-02-20 13:13:14 +0100

    added routing and new pages

--- my-brary/src/pages/HomePage.js
+++ my-brary/src/pages/HomePage.js
@@ -1,24 +1,13 @@
 import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Hero from "../components/hero";
+import Navigation from "../components/navigation";
 import "../styles.css";
-import Trending from "../components/Trending";
 
 function HomePage() {
-  const [bookData, setBookData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://openlibrary.org/trending/daily.json")
-      .then((response) => {
-        console.log(response.data.works);
-        setBookData(() => response.data.works);
-      });
-  }, []);
   return (
     <div className="con">
+      <Navigation />
       <Hero />
-      <Trending trending={bookData.slice(0, 12)} />
     </div>
   );
 }
--- my-brary/src/pages/TrendingPage.js
+++ my-brary/src/pages/TrendingPage.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Trending from "../components/Trending";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Navigation from "../components/navigation";
 
 export default function TrendingPage() {
+  const [bookData, setBookData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://openlibrary.org/trending/daily.json")
+      .then((response) => {
+        console.log(response.data.works);
+        setBookData(() => response.data.works);
+      });
+  }, []);
   return (
     <div>
-      <h1>trending page</h1>
+      <Navigation />
+      <Trending trending={bookData.slice(0, 12)} />
     </div>
   );
 }
