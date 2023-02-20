2a2645946a96286a2c2024b11260c1c838a08672
notpritam <119423845+notpritam@users.noreply.github.com>
2023-02-20 17:51:43 +0530

    Baisc Done

--- my-brary/src/pages/TrendingPage.js
+++ my-brary/src/pages/TrendingPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Trending from "../components/Trending";
 
-export default function TrendingPage({data}) {
+export default function TrendingPage() {
   const [bookData, setBookData]=useState([]);
   const trendingUrl = "https://openlibrary.org/trending/daily.json"
 
@@ -17,19 +17,6 @@ export default function TrendingPage({data}) {
         setBookData(() => response.data.works);}
     )
   }
-
-
-
-export default function TrendingPage() {
-  const [bookData, setBookData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://openlibrary.org/trending/daily.json")
-      .then((response) => {
-        console.log(response.data.works);
-        setBookData(() => response.data.works);
-      });
-  }, []);
   return (
     <>
     <Trending  trending={bookData}/>
